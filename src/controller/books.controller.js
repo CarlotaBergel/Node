e12e271506f21631662658eaf7d706ac346bb753
Carlota <carlotabergel7@hotmail.com>
2024-02-11 14:07:46 +0100

    dia4

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -11,18 +11,22 @@ var books = [
 
 function getBooks(req, res){
     let respuesta;
-    let bookFiltro;
     if(req.query.id_book){
-        bookFiltro = books.filter(b => b.id_book == req.query.id_book);
-        respuesta = books;        
+        console.log("LLAMO CON ID_BOOK AL SERVIDOR")
+        respuesta = books.filter(b => b.id_book == req.query.id_book);
     }else{
-        respuesta = "Petición mal formulada";
+        //respuesta = "Petición mal formulada";
+        respuesta = books;
     }
+
+    console.log(respuesta);
     res.send(respuesta);
 }
 
 function postBooks(req, res){
     let respuesta = new book(req.body.title, req.body.type, req.body.author, req.body.price, req.body.photo, req.body.id_book)
+    console.log(respuesta)
+    books.push(respuesta);
     res.status(200).send(respuesta);
 }
 
@@ -31,20 +35,21 @@ function putBooks(req, res){
     if(req.body.id_book && books.findIndex(b => b.id_book == req.body.id_book) >= 0){
         let libro = new book(req.body.title, req.body.type, req.body.author, req.body.price, req.body.photo, req.body.id_book);
         books[books.findIndex(b => b.id_book == req.body.id_book)] = libro;
-        respuesta = "Libro editado";
+        respuesta = {headers: null, body:{"response":"Libro editado"}};
     }else{
-        respuesta = "Libro no encontrado";
+        respuesta = {headers: null, body:{"response":"Libro no encontrado"}};
     }
     res.status(200).send(respuesta);
 }
 
 function deleteBooks(req, res){
     let respuesta;
+    console.log(req);
     if(req.body.id_book && books.findIndex(b => b.id_book == req.body.id_book) >= 0){
         books.splice(books.findIndex(b => b.id_book == req.body.id_book), 1);
-        respuesta = "Libro eliminado";
+        respuesta = {headers: null, body:{"response":"Libro eliminado"}};
     }else{
-        respuesta = "Libro no encontrado";
+        respuesta = {headers: null, body:{"response":"Libro no encontrado"}};
     }
     res.status(200).send(respuesta);
 }
